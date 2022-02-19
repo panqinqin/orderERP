e1aeb3eab13b6710a16e40bea678fb2f77be6470
panqq <panqq@authine.com>
2022-02-19 19:07:41 +0800

    修改

--- src/api/getData.js
+++ src/api/getData.js
@@ -4,7 +4,7 @@ import fetch from '@/config/fetch'
  * 登陆
  */
 
-export const login = data => fetch('/admin/login', data, 'POST');
+export const login = data => fetch('/v1/user/login', data, 'POST');
 
 /**
  * 退出
